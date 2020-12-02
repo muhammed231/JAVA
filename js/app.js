8c8701f431da31df261b0efc486c2c9a43e5d464
muhammed231 <61506461+muhammed231@users.noreply.github.com>
2020-12-02 20:24:24 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,85 @@
+for (let i = 0; i <= 10; i++) {
+    console.log('2 x', i, '=', i * 2)
+}
+
+var name = ''
+
+console.log(name)
+
+for ( let i=0; i<=10; i++) {
+    console.log ('3 x', i, '=', i *3)
+}
+
+var name = ''
+
+console.log(name)
+
+for (let i=0; i<=10; i++) {
+    console.log ('4 x', i, '=', i * 4)
+}
+
+var name = ''
+
+console.log(name)
+
+for (let i=0; i<=10; i++) {
+    console.log ('5 x', i, '=', i * 5)
+}
+
+var name = ''
+
+console.log(name)
+
+for (let i=0; i<=10; i++) {
+    console.log ('6 x', i, '=', i * 6)
+}
+
+var name = ''
+
+console.log(name)
+
+for (let i=0; i<=10; i++) {
+    console.log ('7 x', i, '=', i * 7)
+}
+
+var name = ''
+
+console.log(name)
+
+
+for (let i=0; i<=10; i++) {
+    console.log ('8 x', i, '=', i * 8)
+}
+
+var name = ''
+
+console.log(name)
+
+for (let i=0; i<=10; i++) {
+    console.log ('9 x', i, '=', i * 9)
+}
+
+var name = ''
+
+console.log(name)
+
+for (let i=0; i<=10; i++) {
+    console.log ('10 x', i, '=', i * 10)
+}
+
+var name = ''
+
+console.log(name)
+
+
+for (let i=0; i<=10; i++) {
+    console.log ('11 x', i, '=', i * 11)
+}
+
+var name = ''
+
+console.log(name)
+
+for (let i=0; i<=10; i++) {
+    console.log ('12 x', i, '=', i * 12)
+}
